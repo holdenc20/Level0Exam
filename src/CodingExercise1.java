039e638860cdd4d78bf02cfbe6b9730b77138c19
League Mac <league@WTS15.attlocal.net>
2016-03-15 16:25:35 -0700

    level0 exam

---
+++ src/CodingExercise1.java
@@ -0,0 +1,35 @@
+/* Level 0 Exam: Coding  Exercise #1 */
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Tortoise;
+
+
+public class CodingExercise1
+{
+	public static void main(String[] args)
+	{
+		// 3. ask the user what color they would like the tortoise to draw
+		String color=JOptionPane.showInputDialog("What color do you want?");
+		// 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
+		if(color.equalsIgnoreCase("Blue")){
+			Tortoise.setPenColor(Color.blue);
+		}
+		else if(color.equalsIgnoreCase("Red")){
+			Tortoise.setPenColor(Color.RED);
+		}
+		else{
+			JOptionPane.showMessageDialog(null,"Color not found!");
+		}
+		
+		// 2. set the pen width to 10
+		Tortoise.setPenWidth(10);
+		// 1. make the tortoise draw a shape
+		for(int i=0;i<4;i++){
+		Tortoise.move(100);
+		Tortoise.turn(90);
+		}
+	}
+}
+
---
+++ src/CodingExercise2.java
@@ -0,0 +1,25 @@
+
+/* Level 0 Exam: Coding  Exercise #2 */
+import javax.swing.JOptionPane;
+
+/**
+ * Write a program that asks the user for their age. 
+ * Tell them which year they were born. 
+ * If they are over 30, tell them they are too old to play this game.
+ **/
+
+public class CodingExercise2 {
+	public static void main(String[] args) {
+		
+	
+	String p =JOptionPane.showInputDialog("What is your age?");
+	int x=Integer.parseInt(p);
+	int thisyear=2016;
+	int yearofbirth=thisyear-x;
+	JOptionPane.showMessageDialog(null,"You were born in "+yearofbirth);
+	if(x>30){
+		JOptionPane.showMessageDialog(null, "You are too old to play this game!");
+	}
+}}
+
+
